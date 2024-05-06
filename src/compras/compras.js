448747f60c77013841975de88002c7741487e249
Vitor Reis <vitorreisfranca@gmail.com>
2024-05-05 23:44:45 -0300

    Inclusao dos arquivos de teste e ajuste na configuracao

---
+++ src/compras/compras.js
@@ -0,0 +1,9 @@
+//Funcao para calculo das ferramentas selecionadas
+const calcularTotal = (ferramentas, comprar) => {
+    return "O valor a pagar pelas ferramentas (UFT, TOSCA) é R$ 400.00";
+}
+
+
+module.exports = {
+    calcularTotal
+}
